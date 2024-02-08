1033cd4957a317b628b0bdff0735717a6ffd0c32
zamiaswad <zamiaswad@gmail.com>
2024-02-08 22:41:10 +0700

    Add Filters and Tabs to Table

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\EmployeeResource\Pages;
 
-use App\Filament\Resources\EmployeeResource;
 use Filament\Actions;
+use App\Models\Employee;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\EmployeeResource;
+use Filament\Resources\Pages\ListRecords\Tab;
 
 class ListEmployees extends ListRecords
 {
@@ -14,6 +17,22 @@ class ListEmployees extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make(),
+            'This Week' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_of_hire', '>=', now()->subWeek()))
+                ->badge(Employee::query()->where('date_of_hire', '>=', now()->subWeek())->count()),
+            'This Month' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_of_hire', '>=', now()->subMonth()))
+                ->badge(Employee::query()->where('date_of_hire', '>=', now()->subMonth())->count()),
+            'This Year' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('date_of_hire', '>=', now()->subYear()))
+                ->badge(Employee::query()->where('date_of_hire', '>=', now()->subYear())->count()),
         ];
     }
 }
